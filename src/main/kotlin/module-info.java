e8bc6877c1d2f9d9d29950c80dfeb6f9119c5701
000brian <brianmaciejwilk@gmail.com>
2024-10-29 22:53:14 -0500

    changed template

--- src/main/kotlin/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.premiumpoker {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires kotlin.stdlib;
-
-
-    opens org.example.premiumpoker to javafx.fxml;
-    exports org.example.premiumpoker;
-}
